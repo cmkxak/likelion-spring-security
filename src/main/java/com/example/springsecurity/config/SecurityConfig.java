c86b528c3c62e786a4a8254f7eb889903e696495
cmkxak <gcm466@naver.com>
2022-12-06 15:50:20 +0900

    [add] : Security 설정 클래스 추가

--- src/main/java/com/example/springsecurity/config/SecurityConfig.java
+++ src/main/java/com/example/springsecurity/config/SecurityConfig.java
@@ -1,5 +1,45 @@
 package com.example.springsecurity.config;
 
+import com.example.springsecurity.service.UserService;
+import com.example.springsecurity.utils.JwtTokenFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
 public class SecurityConfig {
 
+    private final UserService userService;
+
+    @Value("${jwt.token.secret}")
+    private String secretKey;
+
+    /**
+     * SecurityFilterChain을 반환하고 빈으로 등록함으로써,
+     * 컴포넌트 기반의 보안 설정이 가능
+     */
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .httpBasic().disable() //token 인증으로 진행할 것이기 때문에 disable로 설정하기
+                .csrf().disable()
+                .cors().and()
+                .authorizeRequests()
+                .antMatchers("/api/v1/users/login", "api/v1/users/join").permitAll()
+                .antMatchers(HttpMethod.POST, "/api/v1/**").authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+
+                .addFilterBefore(new JwtTokenFilter(userService, secretKey), UsernamePasswordAuthenticationFilter.class)
+                .build();
+    }
 }
